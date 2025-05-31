35c20203e56bf0803e201c128e005320533ad84b
moustapha-droid2010 <moustapha2010xx@gmail.com>
2025-05-31 18:51:49 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=wxYSxDxK#yKQ-RraLxVC6EWxqvnzsbXWKAGVbHjeEEBrcrdHKRyY",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=EgwS3R4B#eldpN9K8sfjIAzLsTA7qiJvYp-UJJMmiYmKQnBtSR3g",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
